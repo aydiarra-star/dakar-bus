88b219a23f98ff35a2490ca20400ac16c4ace784
aydiarra-star <aydiarra@gmail.com>
2026-09-11 16:24:35 +0000

    Deploying to gh-pages from @ aydiarra-star/dakar-bus@9d7f858662328eced854e9cc0c0ddda379003736 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2098911033" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1688890318" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
