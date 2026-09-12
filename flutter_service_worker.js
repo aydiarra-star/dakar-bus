cdd430c052373d1fc1e1ea6c0028c35aff97c17d
aydiarra-star <aydiarra-star@users.noreply.github.com>
2026-09-12 05:44:34 +0000

    deploy: 17677e128e0156e50c8fd73bfeb176a13b066be0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "52cb191df87d917b1a3ca18e3e918f64",
-"main.dart.js": "6030264788498936156945eecd16e5f7",
+"main.dart.js": "3f9ef070c82cf8d1158e25c5caa97061",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/chromium/canvaskit.wasm": "f5934e694f12929ed56a671617acd254",
 "canvaskit/chromium/canvaskit.js": "43787ac5098c648979c27c13c6f804c3",
