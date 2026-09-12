578b89cd195cf19221889c358b07f96538c55083
aydiarra-star <aydiarra-star@users.noreply.github.com>
2026-09-12 16:58:58 +0000

    deploy: e8313e2c22f5bf52d34c3340b51e1a5575994c76

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "52cb191df87d917b1a3ca18e3e918f64",
-"main.dart.js": "16746e70ca9a25e4dbdcda1c027979bd",
+"main.dart.js": "11756fe28f3556ae0d154de1c4917fcf",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/chromium/canvaskit.wasm": "f5934e694f12929ed56a671617acd254",
 "canvaskit/chromium/canvaskit.js": "43787ac5098c648979c27c13c6f804c3",
@@ -18,7 +18,7 @@ const RESOURCES = {"version.json": "52cb191df87d917b1a3ca18e3e918f64",
 "index.html": "0ffaed8fc3cd53003a000db216810d55",
 "/": "0ffaed8fc3cd53003a000db216810d55",
 "assets/AssetManifest.bin": "a53491ba33870e68dce7c820b0867cda",
-"assets/fonts/MaterialIcons-Regular.otf": "0f025d41272fd2ce8acdda0a92ed0e68",
+"assets/fonts/MaterialIcons-Regular.otf": "bb57cd6f592c18759f74a5daba7c4aeb",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "bb5daa477ab092a8c1a34a960dc05d3a",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
